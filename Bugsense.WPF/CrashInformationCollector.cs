e576ad3d55c1bc0eab0dbe7af941a3f81bb8027f
George Hahn <Twitter: @George_Hahn>
2012-12-11 14:30:57 -0500

    Added username reporting
    Reports username inside the PhoneModel field

--- Bugsense.WPF/CrashInformationCollector.cs
+++ Bugsense.WPF/CrashInformationCollector.cs
@@ -4,52 +4,53 @@ using System.Text;
 
 namespace Bugsense.WPF
 {
-    internal class CrashInformationCollector
-    {
-        private readonly string _version;
+	internal class CrashInformationCollector
+	{
+		private readonly string _version;
 
-        public CrashInformationCollector(string version)
-        {
-            _version = version;
-        }
-        
-        public BugSenseRequest CreateCrashReport(Exception exception)
-        {
-            var entryAssemblyName = Assembly.GetEntryAssembly().GetName();
-            var operatingSystem = Environment.OSVersion;
+		public CrashInformationCollector(string version)
+		{
+			_version = version;
+		}
 
-            var fullStacktrace = GetStackTrace(exception);
+		public BugSenseRequest CreateCrashReport(Exception exception)
+		{
+			var entryAssemblyName = Assembly.GetEntryAssembly().GetName();
+			var operatingSystem = Environment.OSVersion;
 
-            return new BugSenseRequest(
-                new BugSenseEx
-                    {
-                        ExceptionType = exception.GetType().ToString(),
-                        Message = exception.Message,
-                        DateOccured = DateTime.UtcNow,
-                        StackTrace = fullStacktrace
-                    },
-                new AppEnvironment
-                    {
-                        AppName = entryAssemblyName.Name,
-                        AppVersion = _version ?? entryAssemblyName.Version.ToString(4),
-                        OsVersion = operatingSystem.Version.ToString(4)
-                    }
-                );
-        }
+			var fullStacktrace = GetStackTrace(exception);
 
-        private static string GetStackTrace(Exception exception)
-        {
-            var sb = new StringBuilder();
-            var ex = exception;
-            sb.AppendLine(string.IsNullOrEmpty(ex.StackTrace) ? "not available" : ex.StackTrace);
-            ex = ex.InnerException;
-            while (ex != null)
-            {
-                sb.AppendLine(string.Format("Caused by: {0}: {1}", ex.GetType().Name, ex.Message));
-                sb.AppendLine(string.IsNullOrEmpty(ex.StackTrace) ? "not available" : ex.StackTrace);
-                ex = ex.InnerException;
-            }
-            return sb.ToString();
-        }
-    }
+			return new BugSenseRequest(
+				new BugSenseEx
+					{
+						ExceptionType = exception.GetType().ToString(),
+						Message = exception.Message,
+						DateOccured = DateTime.UtcNow,
+						StackTrace = fullStacktrace
+					},
+				new AppEnvironment
+					{
+						AppName = entryAssemblyName.Name,
+						AppVersion = _version ?? entryAssemblyName.Version.ToString(4),
+						OsVersion = operatingSystem.Version.ToString(4),
+						PhoneModel = Environment.UserName
+					}
+				);
+		}
+
+		private static string GetStackTrace(Exception exception)
+		{
+			var sb = new StringBuilder();
+			var ex = exception;
+			sb.AppendLine(string.IsNullOrEmpty(ex.StackTrace) ? "not available" : ex.StackTrace);
+			ex = ex.InnerException;
+			while (ex != null)
+			{
+				sb.AppendLine(string.Format("Caused by: {0}: {1}", ex.GetType().Name, ex.Message));
+				sb.AppendLine(string.IsNullOrEmpty(ex.StackTrace) ? "not available" : ex.StackTrace);
+				ex = ex.InnerException;
+			}
+			return sb.ToString();
+		}
+	}
 }
